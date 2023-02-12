58d1e802b5d7002e3b30d1791dd521d237eec4f3
Mez0ry <612315123@mail.ru>
2023-02-12 17:22:38 +0300

    Added entry point

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "Game.hpp"
+#include "CAppSettings/CAppSettings.hpp"
+
+int main(int agrc, char **argv) {
+  char source_folder_path[FILENAME_MAX];
+
+  if (!GetCurrentDir(source_folder_path, sizeof(source_folder_path))) {
+    return errno;
+  }
+
+  CAppSettings::instance().set_SourceFolder(source_folder_path);
+  CAppSettings::instance().LoadSettings();
+
+  Game gm;
+  gm.Play();
+
+  return 0;
+}
